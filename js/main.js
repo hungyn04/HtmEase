0a458435b28d42286c010961d9f3161a070aca84
nthff0407 <www.thuphan@gmail.com>
2022-12-17 03:54:29 +0700

    changed

--- js/main.js
+++ js/main.js
@@ -2,4 +2,16 @@ function fadeIn() {
     var modal = document.querySelector("modal");
     modal.style.animation="fade-in 0.5s both";
 }
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var headerHeight = 15 - winScroll / 5
+    if (headerHeight >= -5) {
+        var hdh = headerHeight;
+    } else {
+        hdh = -5;
+    }
+    document.documentElement.style.setProperty('--header-height', hdh + "px");
+    window.requestAnimationFrame(myFunction);
+}
 window.onbeforeunload = fadeIn;
+myFunction();
